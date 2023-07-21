ea19e7b456599b2efd7b46ada08014479fa05133
reiple <trsprs@gmail.com>
2023-07-21 22:45:26 +0900

    Q4에 대한 쿼리 작성

--- q4.js
+++ q4.js
@@ -1,5 +1,29 @@
-// TODO:
+// 너무 오래 걸려서 ml_ratings.userId, ml_ratings.movieId에 대해서 각각 인덱스 생성함
+// db.ml_ratings.createIndex({userId: 1})
+// db.ml_ratings.createIndex({movieId: 1})
+
+
+var total_cnt = 0;
+var total = 0;
+
+let start_time = new Date();
+db.ml_ratings.find({userId: input_userid}, {movieId: 1, rating: 1}).forEach(
+    function(doc) {
+        total_cnt = total_cnt + 1;
+        var sum_movie_rating = 0.0;
+        var cnt = 0;
+        db.ml_ratings.find({movieId: doc.movieId}, {rating: 1}).forEach(
+            function(doc_rating) {
+                sum_movie_rating = sum_movie_rating + doc_rating.rating;
+                cnt = cnt + 1;
+            }
+        )
+        total = total + (doc.rating - sum_movie_rating / cnt);
+    }
+)
+let end_time = new Date();
 
 
 var bias = (total / total_cnt).toFixed(3);
 print(bias);
+print("수행시간: " + ((end_time - start_time) / 1000.0) + "초");
